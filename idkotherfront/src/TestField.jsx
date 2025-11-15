8c1433f47c4eb822c634d3ddf3db49fdca05b45a
Feather <featherhoshizora@gmail.com>
2025-11-14 18:18:24 -0800

    Update TestField.jsx
    mostly for comment purposes

--- idkotherfront/src/TestField.jsx
+++ idkotherfront/src/TestField.jsx
@@ -1,6 +1,9 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/reactjs.jsx to edit this template
+/**
+ *
+ * @author Feather Hoshizora
+ * 10/09/2025
+ * TestField
+ * Function for input in forms, reused with different variables 
  */
 export default function TestField({ field, type = "text", ...rest }) {
   const id = `input-${field.toLowerCase()}`;
