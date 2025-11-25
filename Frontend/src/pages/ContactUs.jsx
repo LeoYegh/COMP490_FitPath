190b07e722b10df3684cec0fbb7bed872b3af00c
Feather <featherhoshizora@gmail.com>
2025-11-24 16:21:05 -0800

    Update ContactUs.jsx
    consolidating contactus and contact since latter is now redundant

--- Frontend/src/pages/ContactUs.jsx
+++ Frontend/src/pages/ContactUs.jsx
@@ -1,10 +1,11 @@
 //I had the jsdoc stuff here for ages and couldn't figure out why it wouldn't work.
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import '../styles/ContactUs.css';
 //should update this to accomadate multiple forms and templates later but as of right now unsure which email templates would be necessary
 /** 
  *
- * @returns {JSX.Element} ContactUs
+ * @returns {form@var;sendEmail|String} ContactUs
  * @author Feather Hoshizora
  * 11/10/2025
  * Connects to Email JS service to send emails back and forth from the app directly, 
@@ -22,7 +23,7 @@ export const ContactUs = () => {
       })
       .then(
         () => {
-          console.log('sent email');
+          console.log('sent email');//allows for records of emails sent and for debugging
         },
         (error) => {
           console.log('EMAIL THING BROKE', error.text);//hopefully will not be used?
@@ -31,7 +32,14 @@ export const ContactUs = () => {
   };
 
   return (
+          
     <form ref={form} onSubmit={sendEmail}>
+    <div className="contact">
+            <div className='headerContainer'>
+                <h1>Share feedback!</h1>
+            </div>
+
+        </div>
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
@@ -40,8 +48,10 @@ export const ContactUs = () => {
       <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
+    
   );
 };
+export default ContactUs
 /**service_u3bnr7a gmail service id
  *UCgiIJzRHDSOmrJF7 feather emailJS key
  *template_swquodl contact us form
